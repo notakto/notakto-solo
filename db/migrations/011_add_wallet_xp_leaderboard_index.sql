af88106a4e7b9858c25338225a3563483cfce29f
Rakshit Gupta <rakshitg600@gmail.com>
2026-08-29 12:20:41 +0530

    chore(leaderboard): update index creation query to run concurrently to writes

--- db/migrations/011_add_wallet_xp_leaderboard_index.sql
+++ db/migrations/011_add_wallet_xp_leaderboard_index.sql
@@ -1,11 +1,8 @@
+-- +goose NO TRANSACTION
 -- +goose Up
--- +goose StatementBegin
-CREATE INDEX wallet_xp_desc_not_null_idx
+CREATE INDEX CONCURRENTLY wallet_xp_desc_not_null_idx
     ON wallet (xp DESC)
     WHERE xp IS NOT NULL;
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
-DROP INDEX IF EXISTS wallet_xp_desc_not_null_idx;
--- +goose StatementEnd
+DROP INDEX CONCURRENTLY IF EXISTS wallet_xp_desc_not_null_idx;
